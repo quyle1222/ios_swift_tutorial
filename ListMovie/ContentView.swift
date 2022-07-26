3c41080aeecde84ea1f42eaeb3b44987d8413e8e
phuonglnd <83051410+ledongphuong@users.noreply.github.com>
2022-07-26 13:18:05 +0700

    Initial Commit

---
+++ ListMovie/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ListMovie
+//
+//  Created by TuPT on 26/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ListMovie/ListMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListMovieApp.swift
+//  ListMovie
+//
+//  Created by TuPT on 26/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct ListMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
